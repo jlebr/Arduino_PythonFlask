ea19eabfa2acf1fbf9116c033603294ec207d80e
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-12-30 18:49:27 -0500

    feat: Rutas para recibir y enviar datos funcionando

--- main/routes.py
+++ main/routes.py
@@ -1,9 +1,20 @@
 from flask import Blueprint
 from flask import render_template
+from flask import redirect
+from flask import url_for
+from flask import request
 
 main_bp = Blueprint('main', __name__, url_prefix='/main', template_folder='templates', static_folder='static')
 
 # Ruta principal
 @main_bp.route('', methods=['GET'])
 def main():
-    return render_template('index.html')
+    last_value = False
+    return render_template('index.html', last_value=last_value)
+
+# Obteniendo la respuesta del Front
+@main_bp.route('', methods=['POST'])
+def get_data():
+    data = request.form['data']
+    print(data)
+    return redirect(url_for('main.main'))
