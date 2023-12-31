a30f7279f789d711f869f25c336f25ce7fd44bd1
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-12-30 19:25:42 -0500

    feat: Conectado parte de la interface a base de datos

--- main/routes.py
+++ main/routes.py
@@ -4,17 +4,20 @@ from flask import redirect
 from flask import url_for
 from flask import request
 
+from functions import logic
+from functions import database
+
 main_bp = Blueprint('main', __name__, url_prefix='/main', template_folder='templates', static_folder='static')
 
 # Ruta principal
 @main_bp.route('', methods=['GET'])
 def main():
-    last_value = False
+    last_value = database.get_status()
     return render_template('index.html', last_value=last_value)
 
 # Obteniendo la respuesta del Front
 @main_bp.route('', methods=['POST'])
 def get_data():
     data = request.form['data']
-    print(data)
-    return redirect(url_for('main.main'))
+    last_value = logic.switch(data)
+    return render_template('index.html', last_value=last_value)
