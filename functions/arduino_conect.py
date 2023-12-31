71486ba7992f0234d4f4def01c3838eb3f47dec7
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-12-30 23:07:35 -0500

    feat: Solucionado problema con el arduino

--- functions/arduino_conect.py
+++ functions/arduino_conect.py
@@ -15,11 +15,29 @@ def comunicate(value:int) -> bool:
         conexion.write(value.encode())
         print(f'Valor: {value}')
         conexion.close()
-        return True
+        return leer_serial()
 
     except Exception as e:
         print("Error al establecer la conexión:", e)
         return False
+
+def leer_serial():
+
+    puerto_serial = '/dev/ttyUSB0'  # Cambia esto por tu puerto serial específico
+
+    try:
+        conexion_serial = serial.Serial(puerto_serial, 9600, timeout=1)
+        print("Conexión establecida correctamente")
+
+        while True:
+            # Leer datos desde el puerto serie
+            datos = conexion_serial.readline().decode().strip()  # Lee una línea de datos
+
+            if datos:
+                print(f"Dato recibido por serial: {datos}")
+
+    except serial.SerialException as e:
+        print("Error al establecer la conexión:", e)
 
 if __name__ == '__main__':
     print(comunicate(1))
